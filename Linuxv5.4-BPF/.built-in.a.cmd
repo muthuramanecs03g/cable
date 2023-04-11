e6103aaa873e06f7352721576495ced1163f14b1
muthuramanecs03g <muthuramane.cs03g@g2.nctu.edu.tw>
2023-04-11 15:23:47 +0800

    Removed the following files

--- Linuxv5.4-BPF/.built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_samples/bpf/built-in.a := rm -f samples/bpf/built-in.a; ar cDPrST samples/bpf/built-in.a 
--- Linuxv5.4-BPF/.qos.cmd
+++
@@ -1,1 +0,0 @@
-cmd_samples/bpf/qos := gcc   -o samples/bpf/qos samples/bpf/bpf_load.o samples/bpf/qos_user.o   /home/p4user/Muthu/Projects/linux/samples/bpf/../../tools/lib/bpf/libbpf.a -lelf -lmysqlclient
